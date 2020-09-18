762f2c7e822eb46751e6695699d6e26d9953d2e7
RimuruSenpai <66690766+ItzAguscracK@users.noreply.github.com>
2020-09-17 22:30:04 -0300

    Finish

---
+++ src/app.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const db = require('./database/mongoose.js');
+    //Conexion a la DB
+    db.then(() => console.log('DB Connected'));
+    //Crear App
+    const app = express();
+
+    app.use(express.json());
+    app.use(cors());
+    app.use(morgan('dev'));
+    app.use('/api', require('./api/routes/note'));
+    
+    // Error 404 not found
+    app.use((req, res, next) => {
+      const err = new Error('Not found');
+      err.status = 404;
+      next(err);
+    });
+
+    // Errores distintos a 404
+    app.use((err, req, res, next) => {
+      res.status(err.status || 500);
+      res.json({ error: err.message });
+    });
+
+
+    //Alojamiento
+    app.listen(process.env.PORT, () => {
+        console.log("Servidor alojado en el puerto " + process.env.PORT)
+    });
+
+
+
+
